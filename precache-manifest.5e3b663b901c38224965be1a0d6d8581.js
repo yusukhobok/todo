0cf43c2f511538d9b28fb03148bd3dfd4a1c8d81
Yuri <khvyus@gmail.com>
2019-12-14 16:53:46 +1000

    Updates

---
+++ precache-manifest.5e3b663b901c38224965be1a0d6d8581.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efbb9135a5d7ef0408b8ef1e26dd14c7",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "b2d5bd4ff9840c748621",
+    "url": "/todo/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "72dad9b3f57b74d627bd",
+    "url": "/todo/static/js/2.f2e33c18.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todo/static/js/2.f2e33c18.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b2d5bd4ff9840c748621",
+    "url": "/todo/static/js/main.704e4b16.chunk.js"
+  },
+  {
+    "revision": "d11b971a03af0b2cdc3c",
+    "url": "/todo/static/js/runtime-main.560c68ca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo/static/media/logo.5d5d9eef.svg"
+  }
+]);
